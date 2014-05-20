539b1c9fcf6840d8eb0e9a99eada03d2c0b0a1f5
IrmaBalic <irmabalic1@gmail.com>
2014-05-20 13:44:27 +0200

    Eko karakteristie enum

---
+++ jMUP/src/main/java/ba/co/edgewise/enums/EkoKarakteristike.java
@@ -0,0 +1,42 @@
+package ba.co.edgewise.enums;
+
+public enum EkoKarakteristike {
+	
+	Konvencionalno("Konvencionalno"), 
+	Euro_1("Euro 1"), 
+	Euro_2("Euro 2"), 
+	Euro_3("Euro 3"), 
+	Euro_4("Euro 4"), 
+	Euro_5("Euro 5");
+
+	private final String vrsta;
+	
+	private EkoKarakteristike(final String vrsta)
+	{
+		this.vrsta = vrsta;
+	}
+	@Override
+	public String toString() {
+		return this.vrsta;
+	}
+	
+	public static EkoKarakteristike getEkoKarakteristike(String tip)
+	{
+		switch (tip) {
+			case "Konvencionalno":
+				return EkoKarakteristike.Konvencionalno;
+			case "Euro 1":
+				return EkoKarakteristike.Euro_1;
+			case "Euro 2":
+				return EkoKarakteristike.Euro_2;
+			case "Euro 3":
+				return EkoKarakteristike.Euro_3;
+			case "Euro 4":
+				return EkoKarakteristike.Euro_4;
+			case "Euro 5":
+				return EkoKarakteristike.Euro_5;
+			default:
+				return null;
+		}
+	}
+}
