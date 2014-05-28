8703f9e4ce156ced328d9445cd2db8d9e3afbe25
Nermin <ncatic1@etf.unsa.ba>
2014-05-28 17:55:45 +0200

    ponovo salter

--- jMUP/src/main/java/ba/co/edgewise/jmup/mvc/views/SalterskiRadnikView.java
+++ jMUP/src/main/java/ba/co/edgewise/jmup/mvc/views/SalterskiRadnikView.java
@@ -1,8 +1,31 @@
 package ba.co.edgewise.jmup.mvc.views;
 
-// pocetni view za radnika :D
-// u njemu ce se mijenjati izgled svega necega :D
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-public class SalterskiRadnikView {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class SalterskiRadnikView extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	
+
+	/**
+	 * Create the frame.
+	 */
+	public SalterskiRadnikView() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 486, 342);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
 
 }
