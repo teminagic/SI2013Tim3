79c37666c0aafb0ac8ee26a84e9d153c2a476010
Nermin <ncatic1@etf.unsa.ba>
2014-05-28 15:18:53 +0200

    kontrolercici

--- jMUP/src/main/java/ba/co/edgewise/jmup/mvc/controllers/AdminController.java
+++ jMUP/src/main/java/ba/co/edgewise/jmup/mvc/controllers/AdminController.java
@@ -3,6 +3,10 @@ package ba.co.edgewise.jmup.mvc.controllers;
 public class AdminController {
 
 	public void control(){
-		
+		getBtnDetalji1().addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+			}
+		});
 	}
 }
--- jMUP/src/main/java/ba/co/edgewise/jmup/mvc/views/Administrator.java
+++ jMUP/src/main/java/ba/co/edgewise/jmup/mvc/views/Administrator.java
@@ -7,9 +7,22 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import ba.co.edgewise.jmup.components.MeniAdminOpcije;
+import ba.co.edgewise.jmup.components.NaslovnaAdministrator;
+import ba.co.edgewise.jmup.components.OpcijaNaslov;
+import ba.co.edgewise.jmup.components.OpcijaSadrzaj;
+
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 public class Administrator extends JFrame {
 
 	private JPanel contentPane;
+	private MeniAdminOpcije meni;
+	private OpcijaSadrzaj sadrzaj;
+	private NaslovnaAdministrator strana1;
 
 	/**
 	 * Launch the application.
@@ -20,12 +33,39 @@ public class Administrator extends JFrame {
 	 */
 	public Administrator() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 747, 593);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{200};
+		gridBagLayout.rowHeights = new int[]{0};
+		gridBagLayout.columnWeights = new double[]{0.0,1.0};
+		gridBagLayout.rowWeights = new double[]{Double.MIN_VALUE};
+		contentPane.setLayout(gridBagLayout);
 		this.setVisible(true);
+		
+		meni =  new MeniAdminOpcije();
+		GridBagConstraints gbc_meni = new GridBagConstraints();
+		gbc_meni.anchor = GridBagConstraints.NORTH;
+		gbc_meni.fill = GridBagConstraints.HORIZONTAL;
+		contentPane.add(meni, gbc_meni);
+		
+		sadrzaj = new OpcijaSadrzaj("Poćetna");
+		GridBagConstraints gbc_naslov = new GridBagConstraints();
+		gbc_naslov.anchor = GridBagConstraints.NORTH;
+		gbc_naslov.fill = GridBagConstraints.HORIZONTAL;
+		
+		strana1 = new NaslovnaAdministrator();
+		
+		sadrzaj.getPanelSadrzaj().add(strana1, "Početna");
+		
+		contentPane.add(sadrzaj, gbc_naslov);
+		
+		
 	}
 
+	public NaslovnaAdministrator getStrana1() {
+		return strana1;
+	}
 }
