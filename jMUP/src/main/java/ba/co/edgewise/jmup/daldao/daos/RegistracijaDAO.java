3fcb49558751057d52efc5ce6f9a5c9c9449f8a2
Nermin <ncatic1@etf.unsa.ba>
2014-05-30 01:11:47 +0200

    a

--- jMUP/src/main/java/ba/co/edgewise/jmup/daldao/daos/RegistracijaDAO.java
+++ jMUP/src/main/java/ba/co/edgewise/jmup/daldao/daos/RegistracijaDAO.java
@@ -200,7 +200,7 @@ public class RegistracijaDAO implements IGenericDAO<Registracija, Integer> {
 			ConnectionManager.closeConnection(connection);
 		}
 		
-		return success;
+		return success;//
 	}
 	/*
 	public static void main(String[] args) throws ParseException {
