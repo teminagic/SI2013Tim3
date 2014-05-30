f9d11122fe95399e32ef0c4d37e733a8e7f178c7
adizdar1@hotmail.com <dizdar.ahmed@gmail.com>
2014-05-30 21:17:48 +0200

    pom

--- jMUP/src/main/java/ba/co/edgewise/jmup/mvc/models/BossModel.java
+++ jMUP/src/main/java/ba/co/edgewise/jmup/mvc/models/BossModel.java
@@ -3,42 +3,41 @@ package ba.co.edgewise.jmup.mvc.models;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
+import java.io.FileOutputStream;
 import ba.co.edgewise.jmup.klase.*;
 import ba.co.edgewise.jmup.daldao.daos.*;
-import java.io.FileOutputStream;
+import java.awt.Color;
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.*;
 
-import javax.swing.text.Document;
 
 public class BossModel {
-	
+
 	private VlasnickaDAO _vlasnickaDAO;
 	private SaobracajnaDAO _saobracajnaDAO;
 	private VoziloDAO _voziloDAO;
 	private OsobaDAO _osobaDAO;
 	private MotorDAO _motorDAO;
-	
-	
+
 	public BossModel() {
 		_vlasnickaDAO = new VlasnickaDAO();
 		_saobracajnaDAO = new SaobracajnaDAO();
 		_voziloDAO = new VoziloDAO();
 		_osobaDAO = new OsobaDAO();
 		_motorDAO = new MotorDAO();
-		PdfWriter.
-		Document document = new Document();
 	}
-	
-	public ArrayList<Vozilo> pretragaVozilo( String parametar, String kriterij ) {
-		
-		switch (kriterij){
+
+	public ArrayList<Vozilo> pretragaVozilo(String parametar, String kriterij) {
+
+		switch (kriterij) {
 		case "Registarska oznaka":
 			ArrayList<Vozilo> v = new ArrayList<Vozilo>();
 			Vozilo v1 = _voziloDAO.getByReg(parametar);
 			v.add(v1);
 			return (v == null) ? null : v;
 		case "Godina proizvodnje":
-			ArrayList<Vozilo> v2 = _voziloDAO.getByDate(Integer.parseInt(parametar));
+			ArrayList<Vozilo> v2 = _voziloDAO.getByDate(Integer
+					.parseInt(parametar));
 			return (v2 == null) ? null : v2;
 		case "Vrsta vozila":
 			ArrayList<Vozilo> v3 = _voziloDAO.getByType(parametar);
@@ -47,13 +46,13 @@ public class BossModel {
 			ArrayList<Vozilo> v4 = _voziloDAO.getByMark(parametar);
 			return (v4 == null) ? null : v4;
 		}
-		
+
 		return null;
 	}
-	
-	public ArrayList<Osoba> pretragaVozac( String parametar, String kriterij ) {
-		
-		switch(kriterij){
+
+	public ArrayList<Osoba> pretragaVozac(String parametar, String kriterij) {
+
+		switch (kriterij) {
 		case "Ime":
 			ArrayList<Osoba> o = _osobaDAO.getByIme(parametar);
 			return (o == null) ? null : o;
@@ -61,109 +60,126 @@ public class BossModel {
 			ArrayList<Osoba> o1 = _osobaDAO.getByPrezime(parametar);
 			return (o1 == null) ? null : o1;
 		case "JMBG":
-			// iako dobijam samo 1 osobu zbog povratne vrijednosti metode vracam listu (da sve svedem na jednu metodu)
-			ArrayList<Osoba> o2 = new ArrayList<>(); 
+			// iako dobijam samo 1 osobu zbog povratne vrijednosti metode vracam
+			// listu (da sve svedem na jednu metodu)
+			ArrayList<Osoba> o2 = new ArrayList<>();
 			o2.add(_osobaDAO.getByJMBG(parametar));
 			return (o2 == null) ? null : o2;
 		}
 		return null;
 	}
-	
-	public ArrayList<Saobracajna> pretragaSaobracajna ( String parametar, String kriterij ) {
-		
-		switch(kriterij){
-		//fejlat ce ako se ukuca prezime pa ime ali nemam vremena da to optimizujem
-		//logika je lose uradena ali nemma vremena za pametnije
-		case "Ime i prezime":
-		String [] ip = parametar.split(" ");
-		ArrayList<Osoba> temp = _osobaDAO.getByImeiPrezime(ip[0], ip[1]);
-		ArrayList<Saobracajna> s1 = new ArrayList<>();
-		for (Osoba osoba : temp) {
-			s1.add(_saobracajnaDAO.getByName(osoba.getId()));
-		}
-		return (s1 == null) ? null : s1;
 
-		//vraca Array i ako ima samo 1 result zbog povratnog tipa metode
+	public ArrayList<Saobracajna> pretragaSaobracajna(String parametar,
+			String kriterij) {
+
+		switch (kriterij) {
+		// fejlat ce ako se ukuca prezime pa ime ali nemam vremena da to
+		// optimizujem
+		// logika je lose uradena ali nemma vremena za pametnije
+		case "Ime i prezime":
+			String[] ip = parametar.split(" ");
+			ArrayList<Osoba> temp = _osobaDAO.getByImeiPrezime(ip[0], ip[1]);
+			ArrayList<Saobracajna> s1 = new ArrayList<>();
+			for (Osoba osoba : temp) {
+				s1.add(_saobracajnaDAO.getByName(osoba.getId()));
+			}
+			return (s1 == null) ? null : s1;
+
+			// vraca Array i ako ima samo 1 result zbog povratnog tipa metode
 		case "Registarska oznaka":
-		Vozilo vt = _voziloDAO.getByReg(parametar);
-		ArrayList<Saobracajna> s2 = new ArrayList<>();
-		s2.add(_saobracajnaDAO.getByReg(vt.getId()));
-		return (s2 == null) ? null : s2;
-		
+			Vozilo vt = _voziloDAO.getByReg(parametar);
+			ArrayList<Saobracajna> s2 = new ArrayList<>();
+			s2.add(_saobracajnaDAO.getByReg(vt.getId()));
+			return (s2 == null) ? null : s2;
+
 		case "JMBG":
-		Osoba o = _osobaDAO.getByJMBG(parametar);
-		ArrayList<Saobracajna> s3 = new ArrayList<>();
-		s3.add(_saobracajnaDAO.getByName(o.getId()));	
-		return (s3 == null) ? null : s3;
-		
+			Osoba o = _osobaDAO.getByJMBG(parametar);
+			ArrayList<Saobracajna> s3 = new ArrayList<>();
+			s3.add(_saobracajnaDAO.getByName(o.getId()));
+			return (s3 == null) ? null : s3;
+
 		case "Broj potvrde":
-		ArrayList<Saobracajna> s4 = _saobracajnaDAO.getByPotvrda(parametar);
-		return (s4 == null) ? null : s4;
+			ArrayList<Saobracajna> s4 = _saobracajnaDAO.getByPotvrda(parametar);
+			return (s4 == null) ? null : s4;
 		}
-				
+
 		return null;
 	}
-	
-	public ArrayList<Vlasnicka> pretragaVlasnicka ( String parametar, String kriterij ) {
-		
-		switch(kriterij){
-		//fejlat ce ako se ukuca prezime pa ime ali nemam vremena da to optimizujem
-		//logika je lose uradena ali nemma vremena za pametnije
-		case "Ime i prezime":
-		String [] ip = parametar.split(" ");
-		ArrayList<Osoba> temp = _osobaDAO.getByImeiPrezime(ip[0], ip[1]);
-		ArrayList<Vlasnicka> vl1 = new ArrayList<>();
-		for (Osoba osoba : temp) {
-			vl1.add(_vlasnickaDAO.getByVlasnik(osoba.getId()));
-		}
-		return (vl1 == null) ? null : vl1;
 
-		//vraca Array i ako ima samo 1 result zbog povratnog tipa metode
+	public ArrayList<Vlasnicka> pretragaVlasnicka(String parametar,
+			String kriterij) {
+
+		switch (kriterij) {
+		// fejlat ce ako se ukuca prezime pa ime ali nemam vremena da to
+		// optimizujem
+		// logika je lose uradena ali nemma vremena za pametnije
+		case "Ime i prezime":
+			String[] ip = parametar.split(" ");
+			ArrayList<Osoba> temp = _osobaDAO.getByImeiPrezime(ip[0], ip[1]);
+			ArrayList<Vlasnicka> vl1 = new ArrayList<>();
+			for (Osoba osoba : temp) {
+				vl1.add(_vlasnickaDAO.getByVlasnik(osoba.getId()));
+			}
+			return (vl1 == null) ? null : vl1;
+
+			// vraca Array i ako ima samo 1 result zbog povratnog tipa metode
 		case "Registarska oznaka":
-		Vozilo vt = _voziloDAO.getByReg(parametar);
-		ArrayList<Vlasnicka> vl2 = new ArrayList<>();
-		vl2.add(_vlasnickaDAO.getByVozilo(vt.getId()));
-		return (vl2 == null) ? null : vl2;
-		
+			Vozilo vt = _voziloDAO.getByReg(parametar);
+			ArrayList<Vlasnicka> vl2 = new ArrayList<>();
+			vl2.add(_vlasnickaDAO.getByVozilo(vt.getId()));
+			return (vl2 == null) ? null : vl2;
+
 		case "JMBG":
-		Osoba o = _osobaDAO.getByJMBG(parametar);
-		ArrayList<Vlasnicka> vl3 = new ArrayList<>();
-		vl3.add(_vlasnickaDAO.getByVlasnik(o.getId()));	
-		return (vl3 == null) ? null : vl3;
-		
+			Osoba o = _osobaDAO.getByJMBG(parametar);
+			ArrayList<Vlasnicka> vl3 = new ArrayList<>();
+			vl3.add(_vlasnickaDAO.getByVlasnik(o.getId()));
+			return (vl3 == null) ? null : vl3;
+
 		case "Broj potvrde":
-		ArrayList<Vlasnicka> vl4 = new ArrayList<>();
-		vl4.add(_vlasnickaDAO.get(parametar));
-		return (vl4 == null) ? null : vl4;
+			ArrayList<Vlasnicka> vl4 = new ArrayList<>();
+			vl4.add(_vlasnickaDAO.get(parametar));
+			return (vl4 == null) ? null : vl4;
 		}
-				
+
 		return null;
 	}
-	
-	public Motor getMotorByVozilo(Integer id)
-	{
+
+	public Motor getMotorByVozilo(Integer id) {
 		Vozilo temp = _voziloDAO.get(id);
 		return (temp == null) ? null : temp.getMotor();
 	}
-	
-	public void ekstraktToPDF()
-	{
-		
-	}
-	
 
-	/*public static void main(String[] args) {
+	public void ekstraktToPDF() {
+		try {
+			   Document document=new Document();
+		       PdfWriter.getInstance(document,new FileOutputStream("C:\Users\Ahmed\Desktop\test.pdf");
+		       document.open();
+		       PdfPTable tab=new PdfPTable(2);
+		       tab.addCell("Name");
+		       tab.addCell("Address");
+		       document.add(tab);
+		       document.close();
+
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+
 		BossModel b = new BossModel();
 		ArrayList<Vozilo> v = new ArrayList<>();
 		ArrayList<Osoba> o = new ArrayList<>();
 		ArrayList<Saobracajna> s = new ArrayList<>();
 		ArrayList<Vlasnicka> vl = new ArrayList();
-		
-		vl = b.pretragaVlasnicka("77", "Registarska oznaka");
-		//v = b.pretragaVozilo("BMW","Marka vozila");
-		//o = b.pretragaVozac("Dervic", "Prezime");
-		//s = b.pretragaSaobracajna( "b" , "JMBG");
-		
-	}*/
+
+		//vl = b.pretragaVlasnicka("77", "Registarska oznaka");
+		// v = b.pretragaVozilo("BMW","Marka vozila");
+		// o = b.pretragaVozac("Dervic", "Prezime");
+		// s = b.pretragaSaobracajna( "b" , "JMBG");
+		b.ekstraktToPDF();
+
+	}
 
 }
