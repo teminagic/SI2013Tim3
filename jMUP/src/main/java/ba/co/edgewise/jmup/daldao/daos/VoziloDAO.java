fa57bf852a4e4c07d900977d046dce65d84e10e8
adizdar1@hotmail.com <dizdar.ahmed@gmail.com>
2014-05-31 14:02:37 +0200

    VoziloDAO konflikt rjesen

--- jMUP/src/main/java/ba/co/edgewise/jmup/daldao/daos/VoziloDAO.java
+++ jMUP/src/main/java/ba/co/edgewise/jmup/daldao/daos/VoziloDAO.java
@@ -99,9 +99,8 @@ public class VoziloDAO implements IGenericDAO<Vozilo, Integer> {
 							"`OblikKaroserije`,`GodinaProizvodnje` ,`MaxTehnickaDozvoljenaMasa` ,"+
 							"`MasaVozila` , `DopustenaNosivost` , `Motor` , `OdnosSnageIMase` ,"+
 							"`BrojMjestaZaSjedenje` ,`BrojMjestaZaStajanje` ,`BrojMjestaZaLezanje` ," +
-							"`EkoKarakteristikaVozila`,`Katalizator`,`DatumPregleda`,`RegOznaka`,`Status`)" +
-							"`EkoKarakteristikaVozila`,`Katalizator`,`DatumPregleda`,`RegOznaka`,`Status`)" +
-							"VALUES (?,?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
+							"`EkoKarakteristikaVozila`,`Katalizator`,`DatumPregleda`,`RegOznaka`,`Status`)"+
+							"VALUES (?,?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 			
 			statement3.setString(1, vrsta);
 			statement3.setString(2, marka);
