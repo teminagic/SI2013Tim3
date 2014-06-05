873053321cb5a4c2c6f7b5823ab50586cefad5fe
Amra <Amra@188.127.99.54>
2014-06-05 18:40:24 +0200

    konflikt konacno

--- jMUP/src/main/java/ba/co/edgewise/jmup/mvc/controllers/SalterskiRadnikController.java
+++ jMUP/src/main/java/ba/co/edgewise/jmup/mvc/controllers/SalterskiRadnikController.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.sql.Date;
+import java.util.Date;
 import java.text.ParseException;
 import java.util.ArrayList;
 
