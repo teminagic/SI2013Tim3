31f8a9e773b808eeba95f9af3692df0cf55018de
Amra <Amra@188.127.99.54>
2014-06-05 08:07:47 +0200

    konflikt....

--- jMUP/src/main/java/ba/co/edgewise/jmup/mvc/controllers/SalterskiRadnikController.java
+++ jMUP/src/main/java/ba/co/edgewise/jmup/mvc/controllers/SalterskiRadnikController.java
@@ -5,7 +5,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.text.ParseException;
-
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
